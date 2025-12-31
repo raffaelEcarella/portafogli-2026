c17a83e84b5dff6de640fed988d39e9a530426c5
raffaelEcarella <docwindtrecat@gmail.com>
2025-12-31 16:32:32 +0100

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -9,23 +9,13 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => { if (key !== CACHE_NAME) return caches.delete(key); })
-    ))
-  );
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>{ if(key!==CACHE_NAME) return caches.delete(key); }))));
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request)));
 });
